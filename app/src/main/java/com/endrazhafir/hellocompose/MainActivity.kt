20a2caca2720176282e3cbc5445b3ccfc0067f35
Averone30 <zhafirfza@gmail.com>
2025-11-06 00:20:42 +0700

    Initial commit

---
+++ app/src/main/java/com/endrazhafir/hellocompose/MainActivity.kt
@@ -0,0 +1,94 @@
+package com.endrazhafir.hellocompose
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
+import com.endrazhafir.hellocompose.ui.theme.HelloComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HelloComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding -> HelloToastScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HelloToastScreen() {
+
+    val ctx = LocalContext.current
+    var count by rememberSaveable { mutableStateOf(0) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = { Toast.makeText(ctx, "Count $count", Toast.LENGTH_SHORT).show() },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = stringResource(id = R.string.toast))
+        }
+        Box (
+        modifier = Modifier
+            .fillMaxWidth()
+            .weight(1f)
+            .background(color = colorResource(id = R.color.yellow)),
+        contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = count.toString(),
+                fontSize = (64.sp),
+                color = Color.Black,
+                textAlign = TextAlign.Center
+            )
+        }
+
+        Button(
+            onClick = { count++ },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = stringResource(id = R.string.count))
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HelloToastPreview() {
+    HelloToastScreen()
+}
+
