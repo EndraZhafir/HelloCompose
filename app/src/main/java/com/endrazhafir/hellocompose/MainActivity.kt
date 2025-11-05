b0ee316d01a20323e144230949f780d1f470fc55
Averone30 <zhafirfza@gmail.com>
2025-11-06 01:15:14 +0700

    [add] implement rows

--- app/src/main/java/com/endrazhafir/hellocompose/MainActivity.kt
+++ app/src/main/java/com/endrazhafir/hellocompose/MainActivity.kt
@@ -6,8 +6,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -63,11 +65,11 @@ fun HelloToastScreen() {
             Text(text = stringResource(id = R.string.toast))
         }
         Box (
-        modifier = Modifier
-            .fillMaxWidth()
-            .weight(1f)
-            .background(color = colorResource(id = R.color.yellow)),
-        contentAlignment = Alignment.Center
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .background(color = colorResource(id = R.color.yellow)),
+            contentAlignment = Alignment.Center
         ) {
             Text(
                 text = count.toString(),
@@ -82,6 +84,54 @@ fun HelloToastScreen() {
             modifier = Modifier.fillMaxWidth()
         ) {
             Text(text = stringResource(id = R.string.count))
+        }
+    }
+}
+
+@Composable
+fun HelloToastScreenRowButtons() {
+
+    val ctx = LocalContext.current
+    var count by rememberSaveable { mutableStateOf(0) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box (
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .padding(vertical = 16.dp)
+                .background(color = colorResource(id = R.color.yellow)),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = count.toString(),
+                fontSize = (64.sp),
+                color = Color.Black,
+                textAlign = TextAlign.Center
+            )
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Button(
+                onClick = { Toast.makeText(ctx, "Count $count", Toast.LENGTH_SHORT).show() },
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(text = stringResource(id = R.string.toast))
+            }
+            Button(
+                onClick = { count++ },
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(text = stringResource(id = R.string.count))
+            }
         }
     }
 }
